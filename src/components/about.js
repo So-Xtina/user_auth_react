d4ea5ab78cd762d06f1d78ef803b714bd4e1c0b5
So-Xtina <mu7zic2soul@gmail.com>
2018-05-18 15:32:04 -0700

    completed basic pages and navbar

---
+++ src/components/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default props => (
+	<div>
+		<h1 className="center">About Us</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur labore facere cupiditate aperiam
+			dolores hic earum reiciendis quo illo consectetur.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores omnis, minima nostrum, autem perferendis
+			eius velit ad dicta ducimus voluptatibus excepturi eos saepe ipsum amet accusantium est impedit odio sint?
+		</p>
+	</div>
+);
---
+++ src/components/secret_doc.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default props => (
+	<div>
+		<h1 className="center">Top Secret Document</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur labore facere cupiditate aperiam
+			dolores hic earum reiciendis quo illo consectetur.
+		</p>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolore.</p>
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum harum suscipit quidem rem assumenda
+			officiis cum omnis alias voluptatem nam blanditiis neque illo consequuntur maxime eum nesciunt, nemo
+			excepturi natus.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi vitae, architecto expedita a repellat
+			atque voluptatum maxime ratione reprehenderit, enim quaerat cumque nobis vero fuga. Ipsam modi placeat
+			asperiores vero ad similique officia voluptas consequuntur error nisi odio magnam, perferendis cupiditate
+			eaque, a quasi? Fugiat porro commodi optio architecto sunt?
+		</p>
+		<p>Lorem ipsum dolor sit amet.</p>
+	</div>
+);
